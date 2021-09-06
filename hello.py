d40b443610131639777c28577a75d152dd85f3de
JoelKunamalla <kunamallajoel0@gmail.com>
2021-09-06 22:09:29 +0530

    updated

--- hello.py
+++ hello.py
@@ -22,12 +22,11 @@ def output_individual_roll():
     with open("C:\\Users\\joelr\\OneDrive\\Documents\\GitHub\\1901EE32_2021\\tut03\\regtable_old.csv", "r") as f:
         for line in f:
             file1 = line.split(',')
-            
             del file1[4:8]
             del file1[2]
             if (file1[0] =="rollno"):continue
             try: 
-                with open(f"C:\\Users\\joelr\\OneDrive\\Documents\\GitHub\\1901EE32_2021\\tut03\\output_individual_roll\\{file1[0]}.csv"):
+                with open(f"C:\\Users\\joelr\\OneDrive\\Documents\\GitHub\\1901EE32_2021\\tut03\\output_individual_roll\\{file1[0]}.csv" ):
                     with open(f"C:\\Users\\joelr\\OneDrive\\Documents\\GitHub\\1901EE32_2021\\tut03\\output_individual_roll\\{file1[0]}.csv", 'a') as f11:
                         file1=",".join(file1)
                         f11.write(file1)
